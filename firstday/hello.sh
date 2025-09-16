6d71ef28440fd5ea6008d5f3ab59db6b6ca63d99
Muhammad-Harris-25 <muhammad.harris.cs@gmail.com>
2025-09-16 06:25:06 +0000

    firstday work in shell scripting

---
+++ firstday/hello.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This is first file of shell scripting in devops  , 16/09/2025, tuesday
+
+echo "hello dosto , kasy ho , shell scripting ma maza aa rha hy ya ni "
+
+echo " agr aa rha hy tu krty rho , agr ni maza aa rha tu phr b  kro bht km ki chezz hy "
